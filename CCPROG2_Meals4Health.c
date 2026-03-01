2221be197b2013dacccbba6986698daa5656ea62
Jacob Gregorio <gregjacob14@gmail.com>
2026-03-01 21:42:58 +0800

    Created new File

---
+++ CCPROG2_Meals4Health.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * This is to certify that this project is my own work, based on my personal
+ * efforts in studying and applying the concepts learned. I have
+ * constructed the functions and their respective algorithms and
+ * corresponding code by myself. The program was run, tested, and debugged
+ * by my own efforts. I further certify that I have not copied in part or
+ * whole or otherwise plagiarized the work of other students and/or persons.
+ *
+ * Jacob Miguel P. Gregorio, DLSU ID# 12504157
+ * Gabriel Angelo L. De Silva, DLSU ID# 12507083
+ *****************************************************************************/
+
+ /*
+ * Description: A repository of cooking recipes and to provide tools to add recipes, generate shopping list, and to
+recommend menu.
+ * based on the CCPROG2 MP Specifications (AY 2025-2026 T1).
+ * Programmed by: Jacob Miguel P. Gregorio and Gabriel Angelo L. De Silva
+ * Last modified: 3/1/2026
+ * Version: 0.0
+ * [Acknowledgements: N/A]
+ */
+#include <stdio.h>
+
+int main(){
+    printf("Hi");
+    //Hi Gabe
+}
